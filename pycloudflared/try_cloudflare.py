a506cfaee02425f7e76ec01fdc7e9150312a4e9a
Bingsu <ks2515@naver.com>
2023-02-09 21:46:21 +0900

    feat: check running port

--- pycloudflared/try_cloudflare.py
+++ pycloudflared/try_cloudflare.py
@@ -4,6 +4,7 @@ import atexit
 import re
 import subprocess
 from pathlib import Path
+from typing import NamedTuple
 
 from .util import download, get_info
 
@@ -11,83 +12,135 @@ url_pattern = re.compile(r"(?P<url>https?://\S+.trycloudflare.com)")
 metrics_pattern = re.compile(r"(?P<url>127.0.0.1:\d+/metrics)")
 
 
-def try_cloudflare(
-    port: int | str, metrics_port: int | str | None = None, verbose: bool = True
-) -> str:
-    """
-    launches 'trycloudflare' cloudflared tunnel on the given port
-    <https://try.cloudflare.com>
+class Urls(NamedTuple):
+    tunnel: str
+    metrics: str
+    process: subprocess.Popen
 
-    Parameters
-    ----------
-    port : int | str
-        port to launch the tunnel on.
-    metrics_port : int | str | None, optional, default None
-        port to launch the metrics server on,
-        if None, cloudflared will use a random port to launch the metrics server on.
-    verbose : bool, default True
-        print the tunnel url and metrics url
 
-    Returns
-    -------
-    str
-        tunnel url
+class TryCloudflare:
+    def __init__(self):
+        self.running: dict[int, Urls] = {}
 
-    Raises
-    ------
-    RuntimeError
-        When cloudflared fails to start
-    """
-    info = get_info()
-    if not Path(info.executable).exists():
-        download(info)
+    def __call__(
+        self,
+        port: int | str,
+        metrics_port: int | str | None = None,
+        verbose: bool = True,
+    ) -> Urls:
+        """
+        launches 'trycloudflare' cloudflared tunnel on the given port
+        <https://try.cloudflare.com>
 
-    args = [
-        info.executable,
-        "tunnel",
-        "--url",
-        f"http://127.0.0.1:{port}",
-    ]
+        Parameters
+        ----------
+        port : int | str
+            port to launch the tunnel on.
+        metrics_port : int | str | None, optional, default None
+            port to launch the metrics server on,
+            if None, cloudflared will use a random port to launch the metrics server on.
+        verbose : bool, default True
+            print the tunnel url and metrics url
 
-    if metrics_port is not None:
-        args += [
-            "--metrics",
-            f"127.0.0.1:{metrics_port}",
+        Returns
+        -------
+        str
+            tunnel url
+
+        Raises
+        ------
+        RuntimeError
+            When cloudflared fails to start
+        """
+        info = get_info()
+        if not Path(info.executable).exists():
+            download(info)
+
+        port = int(port)
+        if port in self.running:
+            urls = self.running[port]
+            if verbose:
+                self._print(urls.tunnel, urls.metrics)
+            return urls
+
+        args = [
+            info.executable,
+            "tunnel",
+            "--url",
+            f"http://127.0.0.1:{port}",
         ]
 
-    if info.system == "darwin" and info.machine == "arm64":
-        args = ["arch", "-x86_64"] + args
+        if metrics_port is not None:
+            args += [
+                "--metrics",
+                f"127.0.0.1:{metrics_port}",
+            ]
 
-    cloudflared = subprocess.Popen(
-        args,
-        stdout=subprocess.DEVNULL,
-        stderr=subprocess.PIPE,
-        encoding="utf-8",
-    )
+        if info.system == "darwin" and info.machine == "arm64":
+            args = ["arch", "-x86_64"] + args
 
-    atexit.register(cloudflared.terminate)
+        cloudflared = subprocess.Popen(
+            args,
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.PIPE,
+            encoding="utf-8",
+        )
 
-    lines = 0
-    tunnel_url = metrics_url = ""
+        atexit.register(cloudflared.terminate)
 
-    while lines < 20:
-        lines += 1
-        line = cloudflared.stderr.readline()
+        lines = 0
+        tunnel_url = metrics_url = ""
 
-        url_match = url_pattern.search(line)
-        metric_match = metrics_pattern.search(line)
-        if url_match:
-            tunnel_url = url_match.group("url")
-        if metric_match:
-            metrics_url = "http://" + metric_match.group("url")
+        lines = 20
+        for _ in range(lines):
+            line = cloudflared.stderr.readline()
 
-        if tunnel_url and metrics_url:
-            break
+            url_match = url_pattern.search(line)
+            metric_match = metrics_pattern.search(line)
+            if url_match:
+                tunnel_url = url_match.group("url")
+            if metric_match:
+                metrics_url = "http://" + metric_match.group("url")
 
-    else:
-        raise RuntimeError("Cloudflared failed to start")
+            if tunnel_url and metrics_url:
+                break
 
-    if verbose:
+        else:
+            raise RuntimeError("Cloudflared failed to start")
+
+        urls = Urls(tunnel_url, metrics_url, cloudflared)
+        if verbose:
+            self._print(urls.tunnel, urls.metrics)
+
+        self.running[port] = urls
+        return urls
+
+    @staticmethod
+    def _print(tunnel_url: str, metrics_url: str) -> None:
         print(f" * Running on {tunnel_url}")
         print(f" * Traffic stats available on {metrics_url}")
-    return tunnel_url
+
+    def terminate(self, port: int | str) -> None:
+        """
+        terminates the cloudflared tunnel on the given port
+
+        Parameters
+        ----------
+        port : int | str
+            port to terminate the tunnel on.
+
+        Raises
+        ------
+        ValueError
+            When the port is not running
+        """
+        port = int(port)
+        if port in self.running:
+            self.running[port].process.terminate()
+            atexit.unregister(self.running[port].process.terminate)
+            del self.running[port]
+        else:
+            raise ValueError(f"port {port!r} is not running.")
+
+
+try_cloudflare = TryCloudflare()
